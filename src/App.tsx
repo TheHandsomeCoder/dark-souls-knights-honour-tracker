0e01ea0446dcdf6a5701f250994efc67387376ed
Scott O'Malley <scottyomalley@gmail.com>
2021-06-26 19:33:05 +0100

    Move item lookup to own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,10 @@
-import React, { SyntheticEvent, useState } from "react";
-import { itemMap } from "./constants/full-item-list";
-import { Form, Table } from "semantic-ui-react";
+import React from "react";
+import { FileDropzone } from "./FileDropzone";
 
 function App() {
-  const itemsAsOptions = Array.from(itemMap.keys()).map((key) => ({
-    key,
-    text: itemMap.get(key)!.name,
-    value: key,
-  }));
-  const [knightsHonourList, setKnightsHonourList ] = useState<string[]>([]);
-  const itemTypeOnChange = (event: SyntheticEvent, data: any) => {
-    setKnightsHonourList(data.value);
-  };
-
   return (
     <div className="App">
-      <Form>
-        <Form.Select
-          label="Item Type"
-          search
-          selection
-          multiple
-          onChange={itemTypeOnChange}
-          value={knightsHonourList}
-          options={itemsAsOptions}
-        />
-      </Form>
-      <Table>
-          <Table.Body>
-            {
-              knightsHonourList.map(i => {
-                const item = itemMap.get(i);
-                return (
-                  <Table.Row>
-                  <Table.Cell>{i}</Table.Cell>
-                  <Table.Cell>{item?.name}</Table.Cell>
-                </Table.Row>
-                )
-              })
-            }
-           
-          </Table.Body>
-      </Table>
+      <FileDropzone />
     </div>
   );
 }
