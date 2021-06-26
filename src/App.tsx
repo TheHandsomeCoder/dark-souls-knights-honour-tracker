b81c5a2666bcf624e75c0ee3321a4180bd658d94
Scott O'Malley <scottyomalley@gmail.com>
2021-06-26 20:06:48 +0100

    Add semantic ui to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { FileDropzone } from "./FileDropzone";
+import { FileDropzone } from "./components/FileDropzone";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
-      <FileDropzone />
+      <Container text>
+        <FileDropzone />
+      </Container>
     </div>
   );
 }
