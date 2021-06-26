a9d03668c6b0a8af2e97b5bb1b4ad0b803fb75e9
Scott O'Malley <scottyomalley@gmail.com>
2021-06-26 19:32:32 +0100

    Fix linting errors with saveParser.js

--- src/util/saveParser.js
+++ src/util/saveParser.js
@@ -1,58 +1,56 @@
 // Copied from https://github.com/gabtoubl/gabtoubl.github.io/blob/master/ds1_save/saveParser.js
 // Updated to fix lint errors
 
-g_idx = 0;
-read = (data, nbBytes) => {
-  if (g_idx > data.length)
-    return "";
-  str = data.substr(g_idx, nbBytes);
+let g_idx = 0;
+const read = (data, nbBytes) => {
+  if (g_idx > data.length) return "";
+  const str = data.substr(g_idx, nbBytes);
   g_idx += nbBytes;
   return str;
-}
+};
 
-skipBytes = (data, nbBytes) => {
+const skipBytes = (data, nbBytes) => {
   g_idx += nbBytes;
-}
+};
 
-readIntLe = (data, nbBytes) => {
+const readIntLe = (data, nbBytes) => {
   var nb = 0;
   for (var i = 0, mult = 1; i < nbBytes; ++i, mult *= 256)
-    nb += data.charCodeAt(i+g_idx) * mult;
+    nb += data.charCodeAt(i + g_idx) * mult;
   g_idx += nbBytes;
   return nb;
-}
+};
 
-parseSaveFile = (data) => {
+export const parseSaveFile = (data) => {
   var fileFormat = read(data, 4);
   skipBytes(data, 20);
   var version = read(data, 8);
-  if (fileFormat != "BND4" || version != "00000001") {
+  if (fileFormat !== "BND4" || version !== "00000001") {
     console.warn("This probably isn't a Dark Souls' save file");
     g_idx = 0;
     return [];
   }
   skipBytes(data, 32);
-  offsets = [];
+  const offsets = [];
   for (var i = 0; i < 10; ++i) {
     skipBytes(data, 16);
     offsets.push(readIntLe(data, 4));
     skipBytes(data, 12);
   }
   return getPlayersSave(data, offsets);
-}
+};
 
-getPlayersSave = (data, offsets) => {
-  players = [];
+const getPlayersSave = (data, offsets) => {
+  const players = [];
   for (var offset of offsets) {
-    var player = {'name':''};
+    var player = { name: "" };
     g_idx = offset + 232;
     player.level = readIntLe(data, 4);
     skipBytes(data, 20);
     for (var i = 0; i < 28; ++i) {
       var c = read(data, 1);
       skipBytes(data, 1);
-      if (c.charCodeAt(0) == 0)
-        break;
+      if (c.charCodeAt(0) === 0) break;
       player.name += c;
     }
     g_idx = offset + 724;
@@ -60,57 +58,58 @@ getPlayersSave = (data, offsets) => {
       skipBytes(data, 12);
       player.items = [];
       while (g_idx < offset + 0xe2c4) {
-        var item = {};
+        let item = {};
         item.type = readIntLe(data, 4);
         item.id = readIntLe(data, 4);
         item.amount = readIntLe(data, 4);
         skipBytes(data, 4);
-        var have = readIntLe(data, 4);
+        let have = readIntLe(data, 4);
         skipBytes(data, 8);
         if (have) {
-          if (item.type == 0 && item.id != 900000) {
+          if (item.type === 0 && item.id !== 900000) {
             item.id = Math.floor(item.id / 1000) * 1000;
             if (item.id < 2000000 || item.id >= 9000000) {
-              item.type = 'weapon';
-              if (item.id == 312000 || item.id == 315000 || item.id == 857000 || item.id == 1053000 || item.id == 1331000 || item.id == 9013000)
+              item.type = "weapon";
+              if (
+                item.id === 312000 ||
+                item.id === 315000 ||
+                item.id === 857000 ||
+                item.id === 1053000 ||
+                item.id === 1331000 ||
+                item.id === 9013000
+              )
                 item.id -= 1000;
-              else if (item.id == 1054000)
-                item.id = 1051000;
+              else if (item.id === 1054000) item.id = 1051000;
               else if (item.id >= 1411000 && item.id <= 1414000)
                 item.id = 1411000;
               else if (item.id >= 1507000 && item.id <= 1510000)
                 item.id = 1507000;
-            }
-            else
-              item.type = 'ammunition';
-          }
-          else if (item.type == 0x10000000 && item.id != 900000 && item.id != 901000 && item.id != 902000 && item.id != 903000) {
-            item.type = 'armor';
+            } else item.type = "ammunition";
+          } else if (
+            item.type === 0x10000000 &&
+            item.id !== 900000 &&
+            item.id !== 901000 &&
+            item.id !== 902000 &&
+            item.id !== 903000
+          ) {
+            item.type = "armor";
             item.id = Math.floor(item.id / 1000) * 1000;
-          }
-          else if (item.type == 0x20000000)
-            item.type = 'ring';
-          else if (item.type == 0x40000000) {
-            if (item.id >= 200 && item.id <= 215)
-              item.id = 200;
-            if (item.id >= 393 && item.id <= 396)
-              item.id = 393;
-            if (item.id < 800 && item.id != 384)
-              item.type = 'item';
-            else if (item.id >= 1000 && item.id < 2000)
-              item.type = 'material';
-            else if (item.id < 3000 || item.id == 384)
-              item.type = 'key_item';
-            else if (item.id < 6000)
-              item.type = 'magic';
-          }
-          else
-            item.type = '';
-          if (item.type != "") {
-            if ((x = player.items.findIndex(elem => elem.id == item.id && elem.type == item.type)) != -1)
-              player.items[x].amount += item.amount;
-            else
-              player.items.push(item);
+          } else if (item.type === 0x20000000) item.type = "ring";
+          else if (item.type === 0x40000000) {
+            if (item.id >= 200 && item.id <= 215) item.id = 200;
+            if (item.id >= 393 && item.id <= 396) item.id = 393;
+            if (item.id < 800 && item.id !== 384) item.type = "item";
+            else if (item.id >= 1000 && item.id < 2000) item.type = "material";
+            else if (item.id < 3000 || item.id === 384) item.type = "key_item";
+            else if (item.id < 6000) item.type = "magic";
+          } else item.type = "";
+          if (item.type !== "") {
+            const x =
+              player.items.findIndex(
+                (elem) => elem.id === item.id && elem.type === item.type
+              ) !== -1;
+            if (x) player.items[x].amount += item.amount;
+            else player.items.push(item);
           }
         }
       }
@@ -119,4 +118,4 @@ getPlayersSave = (data, offsets) => {
   }
   g_idx = 0;
   return players;
-}
+};
