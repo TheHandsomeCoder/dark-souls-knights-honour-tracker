9042c8ae06179e505a7eeb62a2f4062e89bba2d1
Scott O'Malley <scottyomalley@gmail.com>
2021-06-26 20:41:10 +0100

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import { FileDropzone } from "./components/FileDropzone";
+import React, {useState} from "react";
+import { FileDropzone } from "./components/file-dropzone";
 import { Container } from "semantic-ui-react";
 
+interface Character {
+  name: string;
+}
+
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [characters, setCharacters] = useState<Character[]>([])
+
   return (
     <div className="App">
       <Container text>
-        <FileDropzone />
+        <FileDropzone callback={setCharacters} />
       </Container>
     </div>
   );
---
+++ src/components/file-dropzone/FileDropzone.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { parseSaveFile } from "../../util/saveParser";
+import { Container, Icon, Header, Segment } from "semantic-ui-react";
+import "./FileDropzone.css";
+
+interface FileDropzoneProps {
+  callback: (data: any) => void;
+}
+
+export function FileDropzone(props: FileDropzoneProps) {
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      acceptedFiles.forEach((file: any) => {
+        const reader = new FileReader();
+        reader.readAsBinaryString(file);
+        reader.onabort = () => console.log("file reading was aborted");
+        reader.onerror = () => console.log("file reading has failed");
+        reader.onload = () => {
+          // Do whatever you want with the file contents
+          const binaryStr = reader.result;
+          const characters = parseSaveFile(binaryStr);
+          console.log(characters);
+          props.callback(characters);
+        };
+      });
+    },
+    [props]
+  );
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <Container className="file-dropzone">
+      <div {...getRootProps()}>
+        <Segment>
+          <input {...getInputProps()} />
+          <Header as="h2" icon textAlign="center" color="grey">
+            <Icon name="upload" circular />
+            <Header.Content>
+              Drag n Drop you save here or click to choose a file{" "}
+            </Header.Content>
+          </Header>
+        </Segment>
+      </div>
+    </Container>
+  );
+}
