fc7199a66b826d49af52fb558e020a29e76224b7
Scott O'Malley <scottyomalley@gmail.com>
2021-07-19 21:44:47 +0100

    Add tests for determining which file is which

--- src/util/ds1-save-parser/__tests__/index.test.ts
+++ src/util/ds1-save-parser/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { parseSaveFile } from "../";
+import { isDSRemastered, isPrepareToDieSaveFile, parseSaveFile } from "../";
 import {
   DARK_SOULS_PREPARE_TO_DIE_FIXTURE,
   DARK_SOULS_REMASTERED_FIXTURE,
@@ -8,10 +8,18 @@ describe("Dark Souls Prepare To Die Save File Parser", () => {
   it("should not throw an error if the file doesn't start with BND4", () => {
     expect(parseSaveFile(DARK_SOULS_PREPARE_TO_DIE_FIXTURE)).toBeTruthy();
   });
+
+  it("should have a version number of 00000001", () => {
+    	expect(isPrepareToDieSaveFile(DARK_SOULS_PREPARE_TO_DIE_FIXTURE)).toBe(true);
+    	expect(isPrepareToDieSaveFile(DARK_SOULS_REMASTERED_FIXTURE)).toBe(false);
+  });
 });
 
 describe("Dark Souls Remastered Save File Parser", () => {
   it("should not throw an error if the file doesn't start with BND4", () => {
-    expect(parseSaveFile(DARK_SOULS_REMASTERED_FIXTURE)).toBeTruthy();
+    expect(parseSaveFile(DARK_SOULS_REMASTERED_FIXTURE)).toBe(true);
+  });
+  it("should have the file length of a dark souls remastered file", () => {
+    expect(isDSRemastered(DARK_SOULS_REMASTERED_FIXTURE)).toBe(true);
   });
 });
--- src/util/ds1-save-parser/index.ts
+++ src/util/ds1-save-parser/index.ts
@@ -1,21 +1,41 @@
 export const parseSaveFile = (buffer: ArrayBuffer) => {
-    isBND4File(buffer);
-    return true;
-}
-
-export enum DSFileTypes{
-    PREPARE_TO_DIE,
-    REMASTERED,
-    UNKNOWN
-}
-
-const BND4 = 'BND4';
-const isBND4File = (buffer:ArrayBuffer) => {
-    const decoder = new TextDecoder();
-    const firstFourBytes = new Uint8Array(buffer.slice(0, 4));
-    const decodedFirstFourBytes = decoder.decode(firstFourBytes);
-    if(decodedFirstFourBytes !== BND4){
-        throw new Error("File isn't a Dark Souls Prepare to Die or Remastered File");        
-    }
-    return true;    
+  if (!isBND4File(buffer)) {
+    throw new Error(
+      "File isn't a Dark Souls or Dark Souls Remastered Save file"
+    );
+  }
+  return true;
 };
+
+export enum DSFileTypes {
+  PREPARE_TO_DIE,
+  REMASTERED,
+  UNKNOWN,
+}
+
+const REMASTERED_SAVE_FILE_SIZE = 0x4204D0;
+// const SAVE_SLOT_SIZE = 0x060030;
+// const BASE_SLOT_OFFSET = 0x02C0;
+// const USER_DATA_SIZE = 0x060020;
+// const USER_DATA_FILE_CNT = 11;
+// const USER_DATA_FILE_NAME_LEN = 13;
+
+const BND4 = "BND4";
+const isBND4File = (buffer: ArrayBuffer) => {
+  const decoder = new TextDecoder();
+  const firstFourBytes = new Uint8Array(buffer.slice(0, 4));
+  const decodedFirstFourBytes = decoder.decode(firstFourBytes);
+  return decodedFirstFourBytes === BND4;
+};
+
+export const isPrepareToDieSaveFile = (buffer: ArrayBuffer) => {
+    const rawVersion = new Uint8Array(buffer.slice(0x18, 0x20));
+    const decoder = new TextDecoder();
+    const version = decoder.decode(rawVersion);
+    return version === "00000001"
+};
+
+
+export const isDSRemastered = (buffer: ArrayBuffer) => {
+    return buffer.byteLength === REMASTERED_SAVE_FILE_SIZE;
+}
